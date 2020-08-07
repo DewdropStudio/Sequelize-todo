fdd57725961e9839819222e7a6ed291d7837e3a6
Rocio Avila <rocio.avila@northwestern.com>
2020-08-06 20:58:00 -0500

    Part 2

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,27 +4,42 @@
 
 // Dependencies
 // Routes
+
+const db = require('../models');
 // =============================================================
 module.exports = function(app) {
 
     // GET route for getting all of the todos
     app.get("/api/todos", function(req, res) {
-      res.end()
+      db.Todo.findAll().then((data) => {
+          res.json(data);
+      });
     });
   
     // POST route for saving a new todo. We can create a todo using the data on req.body
     app.post("/api/todos", function(req, res) {
-      res.end()
+      db.Todo.create(req.body).then( (data) =>{
+          res.json(data);
+      });
     });
   
     // DELETE route for deleting todos. We can access the ID of the todo to delete in
     // req.params.id
     app.delete("/api/todos/:id", function(req, res) {
-      res.end()
+        db.Todo.destroy({
+            where: { id }
+          })
+          .then(data => res.json(data))
+          .catch(err => { throw err })
     });
   
     // PUT route for updating todos. We can access the updated todo in req.body
-    app.put("/api/todos", function(req, res) {
-      res.end()
+    app.put("/api/todos", async function(req, res) {
+      const { id, text, complete } = req.body
+      const data = await db.Todo.update(
+          { text, complete },
+          { where: { id } }
+      )
+        res.json(data)
     });
   };
